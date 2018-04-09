d65d62fc124d228385287af66499d0a27c1be4c2
oana <o.miron@jacobs-university.de>
2018-04-09 19:02:41 +0200

    added verifications to aux.h

--- simulator/aux.h
+++ simulator/aux.h
@@ -8,37 +8,63 @@ using std::string;
 namespace aux{
     struct tcolor{
         int c; //0 or 1 for black or red 
-        tcolor(int x):c(x){};
+        tcolor(int x){
+            if(x!=0&&x!=1)
+                throw"The color is not in proper boundaries\n";
+            c=x;
+        };
         tcolor():c(0){};
     };
 
     struct tdirection{
         int d;
-        tdirection(int x):d(x){};
+        tdirection(int x){
+            if(x<0||x>5)
+                throw "The direction is out of boundaries\n";
+            d=x;
+        };
         tdirection():d(0){};
     };
 
     struct tsensedir{
         int s;
-        tsensedir(int x):s(x){};
+        tsensedir(int x){
+            if(x<0||x>3)
+                throw "The sense direction is out of boundaries\n";
+            s=x;
+        };
     };
 
     struct tleftright{
         int l;
-        tleftright(int x):l(x){};
+        tleftright(int x){
+            if(x!=0||x!=1)
+                throw "The leftright parameter is out of boundaries\n";
+            l=x;
+        };
     };
 
     struct tmark{
         int m;
         tmark():m(0){};
-        tmark(int x):m(x){};
+        tmark(int x){
+            if(x<0||x>5)
+                throw "The mark is out of boundaries\n";
+            m=x;
+        };
     };
 
     struct tstate{
         int st;
-        tstate(int x):st(x){};
+        tstate(int x):st(x){
+            if(st<0||st>9999)
+                throw "The state is out of boundaries\n";
+        };
         tstate():st(0){};
-        tstate(string x):st(stoi(x)){};
+        tstate(string x):st(stoi(x)){
+            if(st<0||st>9999)
+                throw "The state is out of boundaries\n";
+        };
     };
 
     struct tcondition{
